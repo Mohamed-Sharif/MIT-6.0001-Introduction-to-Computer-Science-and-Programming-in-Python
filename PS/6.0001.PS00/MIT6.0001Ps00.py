c75cf5075db7fec0fecb91fae1ab12020b4e4a93
Mohamed Sharif <68668775+Mohamed-Sharif@users.noreply.github.com>
2022-06-12 01:25:38 +0200

    Delete PS directory

--- PS/6.0001.PS00/MIT6.0001Ps00.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-try:
-    x = int(input('Enter number x:'))
-    y = int(input('Enter number y:'))
-    print ('x**y =',x**y)
-    print ('log ({}) ='.format(x), np.log2(x))
-except:
-    print ('please enter interger numbers')
-
